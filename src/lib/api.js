a016bcd73893107cf7baca300a5ff5c2dd7b3287
unknown <vayneirisu@gmail.com>
2026-05-10 21:39:03 +0700

    update api link

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000/api';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://backend-wms-template-production.up.railway.app/api';
 
 class ApiClient {
   constructor() {
